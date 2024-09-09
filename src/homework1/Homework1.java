985312c6c54d95c46d1d797d2f8246f80fe022c2
kessydy <kessydy@inbox.ru>
2024-09-09 13:52:36 +0300

    homework1

--- src/homework1/Homework1.java
+++ src/homework1/Homework1.java
@@ -1,5 +1,7 @@
+package homework1;
+
 public class Homework1 {
     public static void main(String[] args) {
-    System.out.println("Привет, дз1!");
+        System.out.println("Привет, дз1!");
     }
 }
