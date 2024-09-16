344bff6a8577ae9600f672b91bdf2e8e4a6d5c32
kessydy <kessydy@inbox.ru>
2024-09-16 21:05:53 +0300

    homework2 change

--- src/by/it_academy/homework2/Task7.java
+++ src/by/it_academy/homework2/Task7.java
@@ -2,8 +2,8 @@ package by.it_academy.homework2;
 
 public class Task7 {
     public static void main (String [] args) {
-        int a = 45, b = 34, c = 68, d = 765, e = 343;
-        int f = (a + b + c + d + e)/5;
+        double a = 451, b = 343, c = 682, d = 765, e = 343;
+        double f = (a + b + c + d + e)/5;
         System.out.println("Среднее арифметическое пяти данных чисел составляет " + f);
     }
 }
